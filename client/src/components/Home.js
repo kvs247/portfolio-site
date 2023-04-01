b2d34c8f3fc1301d050e69fc19c2031a2572bb3e
Kyle Schneider <kylesch115@gmail.com>
2023-04-01 16:46:08 -0600

    create skills div on Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,25 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import selfie_jpg from '../assets/selfie.jpg';
+import python_img from '../assets/python.png';
+import flask_img from '../assets/flask.png';
+import javascript_img from '../assets/javascript.png';
+import react_img from '../assets/react.png';
+import html_img from '../assets/html.png';
+import css_img from '../assets/css.png';
+
+const technologies = [
+    python_img, 
+    flask_img, 
+    javascript_img, 
+    react_img, 
+    html_img, 
+    css_img 
+  ]
+
+const tech_list = technologies.map(url => {
+    return <img key={url} src={url}/>
+});
 
 function Home () {
     return (
@@ -16,6 +35,18 @@ function Home () {
                 <Link to='/about'>Click</Link> to learn more about who I am.
             </p>
           </IntroDiv>
+
+          <TechnologiesDiv>
+            <h2>Skills</h2>
+            <div>
+                {tech_list}
+            </div>
+          </TechnologiesDiv>
+
+          <ProjectsDiv>
+
+          </ProjectsDiv>
+
         </>
     );
 }
@@ -30,4 +61,27 @@ const IntroDiv = styled.div`
         height: 200px;
         border: 1px solid var(--white);
     }
+`
+
+const TechnologiesDiv = styled.div`
+    border: 5px solid var(--white);
+    width: 50%;
+    margin: auto;
+    text-align: center;
+
+    div {
+        width: 80%;
+        margin: auto;
+    }
+    
+    img {
+        width: 100px;
+        margin: 10px;
+    }
+`
+
+const ProjectsDiv = styled.div`
+    border: 5px solid var(--white);
+    width: 75%;
+    margin: 25px auto;
 `
