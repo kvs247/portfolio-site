65ad3b60ebcfdc56faeab3062d2dcfc992c547f0
Kyle Schneider <kylesch115@gmail.com>
2023-04-01 17:28:48 -0600

    setup for Render deployment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,20 @@
-from flask import Flask, make_response
+import os
+
+from flask import Flask, make_response, render_template
 from flask_restful import Api, Resource
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+@app.route('/')
+@app.route('/<int:id>')
+def index(id=0):
+    return render_template('index.html')
+
 CORS(app)
 
 app.json.compact = False
