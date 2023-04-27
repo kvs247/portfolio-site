5b5c3654c512c40374280df9c2bf757fc26c75ed
Kyle Schneider <kylesch115@gmail.com>
2023-04-26 21:53:10 -0600

    fix for deployment

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 
 import ProjectCard from './ProjectCard';
 
