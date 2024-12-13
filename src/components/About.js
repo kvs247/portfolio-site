625cb811fc3718d91f9fac2092c4717be97bff90
Kyle <kylesch115@gmail.com>
2024-12-12 17:19:38 -0700

    update skills and about

--- src/components/About.js
+++ src/components/About.js
@@ -23,11 +23,7 @@ function About() {
       <Typography variant='h2' textAlign='center'>About</Typography>
       <Box>
         <Typography color='#e1e1e1'>
-          As a dedicated and self-motivated software developer and researcher, I am constantly seeking out new challenges that allow me to grow and learn. With a strong background in mathematics and science, I am passionate about using technology to solve complex problems and make a positive impact in the world. I thrive on innovation and creativity, and I am committed to pushing the boundaries of what is possible through my work.
-
-          Whether I am conducting research, developing software solutions, or exploring new fields of knowledge, I am always seeking out opportunities to expand my skills and expertise. As someone who thrives on challenge, I am constantly pushing myself to learn and grow in my work. With a keen eye for detail and a results-driven mindset, I am able to deliver exceptional work that meets the needs of my clients and collaborators.
-
-          As a lifelong learner, I am constantly seeking out new ideas and experiences, and I am eager to connect with others who share my passion for intellectual curiosity and personal growth. If you're looking for a dedicated and driven software developer and researcher who is always up for a challenge, then look no further than me.
+          As a software developer with foundations in scientific computing and full-stack development, I combine expertise in JavaScript, Python, and C++ with analytical rigor. My experience includes scientific data analysis, optimizing simulations (95\% runtime reduction) and architecting cloud applications, with a personal focus on type safety, performance, and maintainability. Throughout my career I have balanced technical quality with clear communication to align solutions with team priorities and business requirements.
         </Typography>
       </Box>
       <Box>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,110 +4,161 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
 // technology logos
-import python_img from '../assets/technologies/python.png';
-import flask_img from '../assets/technologies/flask.png';
-import javascript_img from '../assets/technologies/javascript.png';
-import react_img from '../assets/technologies/react.png';
-import html_img from '../assets/technologies/html.png';
-import css_img from '../assets/technologies/css.png';
-import mui_img from '../assets/technologies/mui.png';
-import numpy_img from '../assets/technologies/numpy.png';
-import pandas_img from '../assets/technologies/pandas.png';
+import aws_img from '../assets/technologies/aws.png'
+import bash_img from '../assets/technologies/bash.png'
+import c_img from '../assets/technologies/c.png'
+import cmake_img from '../assets/technologies/cmake.png'
+import cpp_img from '../assets/technologies/cpp.png'
+import dynamodb_img from '../assets/technologies/dynamodb.png'
 import github_img from '../assets/technologies/github2.png';
-import node_img from '../assets/technologies/node.png';
-import render_img from '../assets/technologies/render.png';
+import html_img from '../assets/technologies/html.png';
+import javascript_img from '../assets/technologies/javascript.png';
+import lambda_img from '../assets/technologies/lambda.png'
+import matplotlib_img from '../assets/technologies/matplotlib.png'
+import numpy_img from '../assets/technologies/numpy.png';
+import oop_img from '../assets/technologies/oop.png'
+import pandas_img from '../assets/technologies/pandas.png';
+import postgresql_img from '../assets/technologies/postgresql.png'
+import postman_img from '../assets/technologies/postman.png'
+import python_img from '../assets/technologies/python.png';
+import rds_img from '../assets/technologies/rds.png'
+import react_img from '../assets/technologies/react.png';
+import redux_img from '../assets/technologies/redux.png'
+import s3_img from '../assets/technologies/s3.png'
+import typescript_img from "../assets/technologies/typescript.png"
+import vscode_img from '../assets/technologies/vscode.png'
 
 const gridItem = (name, path) => {
-    return (
-        <Container
-          sx={{
-            width: '15rem',
-            height: '8rem',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            bgcolor: '#292929',
-            m: 'auto',
-            p: 1,
-            borderRadius: '10px',
-            transition: '0.5s',
-            '&:hover': { transform: 'scale(1.05)' }
-          }}
-        >
-          <Box
-            component='img'
-            src={path}
-            alt={`${name} logo`}
-            height='5rem'
-            mr={2}
-          />
-          <Typography color='#e1e1e1'>{name}</Typography>
-        </Container>
-    );
+  return (
+    <Container
+      sx={{
+        width: '15rem',
+        height: '8rem',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bgcolor: '#292929',
+        m: 'auto',
+        p: 1,
+        borderRadius: '10px',
+        transition: '0.5s',
+        '&:hover': { transform: 'scale(1.05)' }
+      }}
+    >
+      <Box
+        component='img'
+        src={path}
+        alt={`${name} logo`}
+        height='5rem'
+        mr={2}
+      />
+      <Typography color='#e1e1e1'>{name}</Typography>
+    </Container>
+  );
 };
 
-function Skills () {
-    return (
-        <Container 
-          maxWidth='none'
-          sx={{
-            display: 'flex', 
-            flexDirection: 'column', 
-            alignItems: 'center',
-            width: '75%',
-            mx: 'auto',
-            mb: '25vh'
-          }}
-        >
-          <Typography 
-            id='skills'
-            variant='h2'
-            my={4} 
-          >
-            Skills
-          </Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Grid container spacing={4}>
-              <Grid item xs={3}>
-                {gridItem('Python', python_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('Flask', flask_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('NumPy', numpy_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('Pandas', pandas_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('JavaScript', javascript_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('NodeJS', node_img)}
-              </Grid>              
-              <Grid item xs={3}>
-                {gridItem('React', react_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('Material UI', mui_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('HTML5', html_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('CSS3', css_img)}
-              </Grid>
-              <Grid item xs={3}>
-                {gridItem('GitHub', github_img)}
-              </Grid>    
-              <Grid item xs={3}>
-                {gridItem('Render', render_img)}
-              </Grid>                            
-            </Grid>
-          </Box>
-      </Container>
-    );
+function Skills() {
+  return (
+    <Container
+      maxWidth='none'
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '75%',
+        mx: 'auto',
+        mb: '25vh'
+      }}
+    >
+      <Typography
+        id='skills'
+        variant='h2'
+        my={4}
+      >
+        Skills
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Grid container spacing={4}>
+          {/* Change all Grid items to use xs={2.4} */}
+          <Grid item xs={2.4}>
+            {gridItem('JavaScript', javascript_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('TypeScript', typescript_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('React', react_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Redux', redux_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('HTML & CSS', html_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Python', python_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Matplotlib', matplotlib_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('NumPy', numpy_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Pandas', pandas_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Python typing', python_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('AWS', aws_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Lambda', lambda_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('DynamoDB', dynamodb_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('RDS', rds_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('S3', s3_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('GitHub', github_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('PostgreSQL', postgresql_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Bash', bash_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('VS Code', vscode_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('Postman', postman_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('C++', cpp_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('C', c_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('CMake', cmake_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('OOP', oop_img)}
+          </Grid>
+          <Grid item xs={2.4}>
+            {gridItem('STL', cpp_img)}
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
 }
 
 export default Skills;
